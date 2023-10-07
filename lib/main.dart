56ba3e8c158a4301ef71690b49307316caefe6d9
masterShanto <mastershanto@gmail.com>
2023-10-07 23:52:30 +0600

    Initial Assignment Code for Module-9

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "My Bag",
+      title: "Assignment for Module-9",
       home: HomeScreen(),
     );
   }
